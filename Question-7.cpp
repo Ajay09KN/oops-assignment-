3ed8fd11949e144f428eaa51f266890f50bd93e1
Ajay Naroliya <166037578+Ajay09KN@users.noreply.github.com>
2024-05-22 20:24:25 +0530

    Update Question-7.cpp

--- Question-7.cpp
+++ Question-7.cpp
@@ -1,1 +1,37 @@
+/*Design a constructor for bank account class.*/
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BankAccount {
+private:
+    string depositorName;
+    long accountNumber;
+    string accountType;
+    double balance;
+
+public:
+    BankAccount(const string& name, long accNumber, const string& accType, double initialBalance) {
+        depositorName = name;
+        accountNumber = accNumber;
+        accountType = accType;
+        balance = initialBalance;
+    }
+
+    void display() {
+        cout << "Depositor Name: " << depositorName << endl;
+        cout << "Account Number: " << accountNumber << endl;
+        cout << "Account Type: " << accountType << endl;
+        cout << "Balance: $" << balance << endl;
+    }
+};
+
+int main() {
+    BankAccount account("Ajay Kumar Naroliya", 123456777, "Savings", 5000.0);
+
+    account.display();
+
+    return 0;
+}
