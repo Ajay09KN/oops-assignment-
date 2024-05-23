f4cdd5814ab70e4aeb8280fa99c828037525631a
Ajay Naroliya <166037578+Ajay09KN@users.noreply.github.com>
2024-05-23 12:54:30 +0530

    Update Question-3.cpp

--- Question-3.cpp
+++ Question-3.cpp
@@ -1,4 +1,4 @@
-/*Write a function using reference variables as arguments to swap the values of a pair of integers.*/
+/* 3.Write a function using reference variables as arguments to swap the values of a pair of integers.*/
 
 #include <iostream>
 
